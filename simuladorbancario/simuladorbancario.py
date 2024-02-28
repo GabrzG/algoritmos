cd1eec588c840cfc68464c66d515e0f3b46358b4
Gabrz <160884747+GabrzG@users.noreply.github.com>
2024-02-28 12:46:48 -0500

    Update simuladorbancario.py
    putos

--- simuladorbancario/simuladorbancario.py
+++ simuladorbancario/simuladorbancario.py
@@ -1,6 +1,31 @@
+from cuentaahorros import cuentaahorros
+from cuentacorriente import cuentacorriente
+from cdt import cdt
+
 class simuladorbancario:
     #codigo
     cedula=""
     nombre=""
     mesActual=""
+    """-----------------------------------------------------------
+    # Asociaciones
+    -----------------------------------------------------------"""
+
+    SaldoCuentaCorriente= cuentacorriente()
+    SaldoCuentaAhorros= cuentaahorros()
+    
+    """---------------------------------------------------------
+    # Metodos
+    ---------------------------------------------------------"""
+
+    def ConsignarValor(self):
+        #Aqui va el codigo
+        return self 
+    
+    def RetirarValor(self):
+        #Aqui va el codigo
+        #ValorRetirar - saldo= saldo
+        return ""
+                
+
     
